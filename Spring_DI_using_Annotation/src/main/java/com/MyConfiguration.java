9ee5e8bae36f84b082b5a506b4c03359a8a400f3
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2023-08-09 20:38:42 +0530

    Spring DI using Annotation

--- Spring_DI_using_Annotation/src/main/java/com/MyConfiguration.java
+++ Spring_DI_using_Annotation/src/main/java/com/MyConfiguration.java
@@ -7,4 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = {"com"})	// == <context:component-scan base-package="com"></context:component-scan> & enable the @Component annotation
 public class MyConfiguration {
 
+    //  No need to create any @Bean bcoz we are using @Component for each POJOs. 
+    //  @Component takes the responsibility to create beans. 
+  
 }
